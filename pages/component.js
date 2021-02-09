8bde472351090f4837ef1f4e66c22126d7b328b6
Nimna Perera <nimnaperera98@gmail.com>
2021-02-10 02:02:00 +0530

    Update component.js

--- pages/component.js
+++ pages/component.js
@@ -67,7 +67,7 @@ export default function Index({ config, kvMonitors, kvMonitorsLastUpdate }) {
           `}
         </script>
       </Head>
-      <div className="container mx-auto px-4" style="display: flex; align-items: center;">
+      <div className="container mx-auto px-4 min-h-screen" style="display: flex; align-items: center;">
       <a href="https://status.rotaract.social" target="_parent" style="width:100%"><MonitorStatusHeader kvMonitorsLastUpdate={kvMonitorsLastUpdate} /></a>
       </div>
     </div>
